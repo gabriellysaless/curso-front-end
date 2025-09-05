4eabb98d21611f734fe10c4440877e90a1636a47
Gabrielly Sales <gabriellysales13@gmail.com>
2025-09-05 11:38:35 -0300

    Atividade prática melhorada

--- programacao-com-js/programacao-orientada-a-objetos/pratica/script.js
+++ programacao-com-js/programacao-orientada-a-objetos/pratica/script.js
@@ -9,33 +9,32 @@ class Parquimetro {
     inserirValor() {
         const valorDepositado = parseFloat(document.getElementById("valor").value);
         
-        if (isNaN(valorDepositado) || valorDepositado <= 0) {
-            return this.mostrarErro("Informe um valor válido!")
-        } else if(valorDepositado < 1) {
+        if(isNaN(valorDepositado) || valorDepositado < 1) {
             return this.mostrarErro("Deposite um valor acima de R$ 1,00!")
-        } else if(valorDepositado >= 1 && valorDepositado < 1.75){
-            const troco = (valorDepositado - 1).toFixed(2);
-            return this.mostrarMensagem("Tempo: 30 minutos", troco);
-        } else if(valorDepositado >= 1.75 && valorDepositado < 3) {
-            const troco2 = (valorDepositado - 1.75).toFixed(2);
-            return this.mostrarMensagem("Tempo: 60 minutos", troco2);
+        } else if(valorDepositado < 1.75){
+            return this.mostrarMensagem("Tempo: 30 minutos", this.calcularTroco(valorDepositado, 1));
+        } else if(valorDepositado < 3) {
+            return this.mostrarMensagem("Tempo: 60 minutos", this.calcularTroco(valorDepositado, 1.75));
         } else {
-            const troco3 = (valorDepositado - 3).toFixed(2);
-            return this.mostrarMensagem("Tempo: 120 minutos", troco3);
-        };
-    };
+            return this.mostrarMensagem("Tempo: 120 minutos", this.calcularTroco(valorDepositado, 3));
+        }
+    }
+
+    calcularTroco(valor, valorTempo) {
+        return (valor - valorTempo).toFixed(2);
+    }
 
     mostrarErro(msg){
         document.getElementById("resultado").textContent = msg;
         document.getElementById("valor").value = '';
         document.getElementById("troco").textContent = '';
-    };
+    }
 
     mostrarMensagem(msg, troco){
         document.getElementById("resultado").textContent = msg;
         document.getElementById("valor").value = '';
         document.getElementById("troco").textContent = `Troco: ${troco}`;
-    };
+    }
     
 };
 
