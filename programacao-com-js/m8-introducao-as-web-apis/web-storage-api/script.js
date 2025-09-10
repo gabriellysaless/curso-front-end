fc69b71507fbfddb7a4e495f303f55f75576b4cf
Gabrielly Sales <gabriellysales13@gmail.com>
2025-09-10 10:19:29 -0300

    Aula sobre web storage api

---
+++ programacao-com-js/m8-introducao-as-web-apis/web-storage-api/script.js
@@ -0,0 +1,29 @@
+const botaoTema = document.getElementById("botaoTema");
+
+botaoTema.addEventListener("click", () => {
+
+    //Verificar se o usuário já tem um tema pré-definido
+    const temaAtual = localStorage.getItem("tema");
+    //Verificar qual é o tema e inverter
+    const novoTema = temaAtual === "dark" ? "light" : "dark"
+    //Adicionar a classe dark no elemento body
+    document.body.classList.toggle(novoTema);
+    //Salvar as preferencias no localStorage
+    localStorage.setItem("tema", novoTema);
+    //Atualiza o texto do botão
+    botaoTema.textContent = novoTema === "dark" ? '☀︎' : '☾';
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    //Verificar se tem tema salvo
+    const temaSalvo = localStorage.getItem("tema");
+
+    //Se for dark, adiciona a classe e altera o botão
+    if(temaSalvo === "dark"){
+        document.body.classList.add("dark");
+        botaoTema.textContent = '☀︎';
+    } else {
+        //Caso contrário é light e o botão deve ser para mudar para dark
+        botaoTema.textContent = '☾';
+    }
+})
