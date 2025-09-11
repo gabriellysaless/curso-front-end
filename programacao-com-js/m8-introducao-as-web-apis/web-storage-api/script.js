7c16e78d36211eacd47f0199d85a80129eb5f038
Gabrielly Sales <gabriellysales13@gmail.com>
2025-09-11 13:50:05 -0300

    Aula atualizada sobre o tema

--- programacao-com-js/m8-introducao-as-web-apis/web-storage-api/script.js
+++ programacao-com-js/m8-introducao-as-web-apis/web-storage-api/script.js
@@ -4,12 +4,18 @@ botaoTema.addEventListener("click", () => {
 
     //Verificar se o usuário já tem um tema pré-definido
     const temaAtual = localStorage.getItem("tema");
+
     //Verificar qual é o tema e inverter
     const novoTema = temaAtual === "dark" ? "light" : "dark"
+
     //Adicionar a classe dark no elemento body
+
+    document.body.classList.remove(temaAtual); //Existe algumas regras nos navegadores que não apaga a classe antiga, com isso precisa remover uma antes de alterar.
     document.body.classList.toggle(novoTema);
+
     //Salvar as preferencias no localStorage
     localStorage.setItem("tema", novoTema);
+
     //Atualiza o texto do botão
     botaoTema.textContent = novoTema === "dark" ? '☀︎' : '☾';
 })
